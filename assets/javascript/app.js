d36c212c70f3ffc7ef1c15cb262919c98e072255
Gabriel <gabrieldeleon-ny@hotmail.com>
2018-01-15 04:06:51 -0500

    Frame Work

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function (){
+
+	function startgame () {
+		var Score = 0
+		$("#p1firstcard").append("<img src=./assets/images/backofcard.png>")
+		$("#p1secondcard").append("<img src=./assets/images/backofcard.png>")
+		$("#p1thirdcard").append("<img src=./assets/images/backofcard.png>")
+		$("#p2firstcard").append("<img src=./assets/images/backofcard.png>")
+		$("#p2secondcard").append("<img src=./assets/images/backofcard.png>")
+		$("#p2thirdcard").append("<img src=./assets/images/backofcard.png>")
+	}
+	startgame()
+})
